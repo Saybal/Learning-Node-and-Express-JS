b8ba63139cd8fa39495221c8daf711af3aeb8611
Saybal <roysaybal@gmail.com>
2026-03-21 11:46:04 +0600

    enterinf to .env and .gitignore

--- Express/index.js
+++ Express/index.js
@@ -1,6 +1,10 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 
 // app.get
 // app.post
@@ -50,7 +54,7 @@ app.get('/user/:id/house/:house', (req, res) => {
     }
 })
 
-app.listen(3000, () => {
-    console.log("Server is running in port 5000");
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
     
 })
